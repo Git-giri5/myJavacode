a41576491472caaf560d8301c5b694ac8e9a46f5
Git-giri5 <program102015@gmail.com>
2026-07-07 17:31:32 +0530

    Adding + like * patt

--- Patt.java
+++ Patt.java
@@ -1,14 +1,11 @@
 import java.util.*;
-public class HolTri {
+public class Patt {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for(int i=0;i<n;i++){
-            for(int j=0;j<n-i;j++){
-                System.out.print(" ");
-            }
-            for(int j=0;j<2*i+1;j++){
-                if(j==0||j==2*i||i==n-1){
+            for(int j=0;j<n;j++){
+                if(i==(n/2)||j==(n/2)){
                     System.out.print("*");
                 }
                 else{
