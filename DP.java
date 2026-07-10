5282b2f7b3f6440e3d75f9e5a893da4aa93596d9
Git-giri5 <program102015@gmail.com>
2026-07-10 20:50:21 +0530

    Adding a code of dynamic prog

---
+++ DP.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class DP {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        System.out.println("Enter the arr value");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int maxx=0,p1=0,p2=0;
+        for(int i=0;i<n;i++){
+            int take=arr[i]+p1;
+            int nottake=p1;
+            maxx=Math.max(take,nottake);
+            p1=maxx;
+            p2=p1;
+        }
+        System.out.println(maxx);
+
+    }
+}
